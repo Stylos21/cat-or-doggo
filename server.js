3d56c7e45c1e19222345b7f6fa70e732dcd7c8f7
elementz <bfdifan21@gmail.com>
2018-12-17 17:43:48 -0500

    Revert to 56e05fced

--- server.js
+++ server.js
@@ -33,5 +33,3 @@ app.get('/data/group1-shard3of4', (req, res) => {
 app.get('/data/group1-shard4of4', (req, res) => {
     res.sendFile(`${__dirname}/public/data/group1-shard1of4`)
 })
-
-// test commit
